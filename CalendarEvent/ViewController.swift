4a49ed1a2424b6481220890e8735d65ee8165e1d
Daniela Palova <daniela.palova@infinno.eu>
2021-04-21 14:50:22 +0300

    Initial commit

---
+++ CalendarEvent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalendarEvent
+//
+//  Created by Daniela Palova on 20.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
